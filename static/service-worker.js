d63199841242391fe705125ebd0e5b9afed87df4
Geoffrey Bell <gtb104@yahoo.com>
2025-12-27 15:54:38 -0500

    chore: update deps and bump version to 2.0.1

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,6 +1,6 @@
 // Service Worker for Freedom Showcase PWA
 // Make sure you update the VERSION if you make any updates!
-const VERSION = 'v2.0.0::';
+const VERSION = 'v2.0.1::';
 const NAME = 'freedom-showcase';
 const CACHE_NAME = `${VERSION}${NAME}`;
 const BASE_PATH = '/SoF_Showcase';
